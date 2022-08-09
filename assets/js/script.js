8dc1c54801503a56adc07e55882f1fe147b3e734
MandyHole <mandyhole17@gmail.com>
2022-08-09 15:12:45 +0000

    Add checkAnswer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,78 @@
-document.addEventListener("DOMContentLoaded", function(){
-let buttons = this.getElementsByTagName("button");
-for (let button of buttons) {
-    button.addEventListener("click", function(){
-        if (this.getAttribute("data-type") === "submit"){
-            alert("you clicked submit")
-        } else {
-            let gameType = this.getAttribute("data-type");
-            alert(`You clicked ${gameType}`)
-        }
-    })
-}
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = this.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                checkAnswer()
+            } else {
+                let gameType = this.getAttribute("data-type");
+                runGame(gameType);
+            }
+        })
+    }
+    runGame("addition")
 })
+/** The main game loop that is run when the script has been loaded
+ * and after a user's answer has been processed.
+ */
+function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting`;
+    }
+}
+/**
+ * Checks user's input against the first value in the
+ * calculateCorrectAnswer array
+ */
 
-function runGame(){
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    if (isCorrect) {
+        alert("Hey you got it right");
+    } else {
+        alert(`Too bad, you answered ${userAnswer} and the correct answer is ${calculatedAnswer[0]}`);
+    }
 
 }
-function checkAnswer(){
+
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator: ${operator}`);
+        throw `Unimplemented operator: ${operator}. Aborting!`;
+    }
+}
+
+function incrementScore() {
 
 }
-function calculateCorrectAnswer(){
+
+function incrementWrongAnswer() {
 
 }
-function incrementScore(){
+
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
-function incrementWrongAnswer(){
+
+function displaySubtractQuestion() {
 
 }
-function displayAdditionQuestion(){
 
-}
-function displaySubtractQuestion(){
-
-}
-function displayMultiplyQuestion(){
+function displayMultiplyQuestion() {
 
 }
